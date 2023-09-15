c2f86db1bffffa589f97efe2da16d57d347887ed
Nickolas Azanov <144555524+BorschtStroganoff@users.noreply.github.com>
2023-09-15 10:45:22 -0400

    Update serialtest.py

--- serialtest.py
+++ serialtest.py
@@ -1,9 +1,13 @@
+# This program was used in testing the servo motors to be used in the EPICS PHARM MAS project.
+# It uses a serial connection to the arduino via the PySerial library.
+# To use this program, you have to install the PySerial libary (https://pypi.org/project/pyserial/)
+# Essentially, you have to type "pip install pyserial" into a command terminal, either from vscode's terminal or thru cmd.exe running as administrator
+
 import serial
 import time
 
 # defines the Serial port and automatically opens the serial connection
 arduino = serial.Serial("COM9", baudrate=9600, timeout=1) # Edit the name of the com port
-print("Hello")
 
 # sends a signal to the arduino via serial connection (USB)
 def command(x):
