a9e382bc60e9a8ba36faedf2a982fd368a20ee05
Nickolas Azanov <144555524+BorschtStroganoff@users.noreply.github.com>
2023-09-15 09:36:56 -0400

    Update servoTest.ino
    This is the version where it is connected to a Python file (i.e. serialtest.py) where the user in the python file can open the servo on command.

--- servoTest.ino
+++ servoTest.ino
@@ -12,6 +12,7 @@
 #define OPEN_ANGLE 90
 
 Servo servo1;  // represents the servo in box 1
+int x;
 
 // Function to open the lid of the box. Argument is the servo we wish to move.
 void open_lid(Servo servo);
@@ -22,14 +23,26 @@ void close_lid(Servo servo);
 
 void setup() {
   servo1.attach(9);  // attaches servo1 to pin 9
+  Serial.begin(9600);
 }
 
 
 void loop() {
-  close_lid(servo1);
-  delay(10000);
-  open_lid(servo1);
-  delay(10000);
+  while(Serial.available() ==0){
+    delay(10);
+  }
+
+  x = Serial.readString().toInt();
+
+  if (x == 1) {
+    open_lid(servo1);
+  }
+
+  if (x == 0) {
+    close_lid(servo1);
+  }
+  
+  delay(2000);
 }
 
 void open_lid(Servo servo)
