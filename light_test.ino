082c273ae7d6bb0e9efc01d2f562031d93ab25b2
Nickolas Azanov <144555524+BorschtStroganoff@users.noreply.github.com>
2024-02-08 21:46:41 -0500

    Update light_test.ino

--- light_test.ino
+++ light_test.ino
@@ -1,39 +1,45 @@
 #include <Adafruit_NeoPixel.h>
 
 #define PIN            6  // The pin to which the Din of the RGB strip is connected
-#define NUMPIXELS      16 // Number of NeoPixels in the strip
+#define NUMPIXELS      16 // Number of pixels in the RGB strip
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   Serial.begin(9600); // Initialize serial communication
-  Serial.println("Setup started");
-  
   strip.begin();
+  delay(100);
   strip.show(); // Initialize all pixels to 'off'
+  Serial.println("Ready to accept inputs");
 }
 
 void loop() {
-  Serial.println("Loop started");
-
-  // Turn on the RGB strip
-  turnOnStrip();
-
-  delay(2000); // Wait for 2 seconds
-
-  // Turn off the RGB strip
-  turnOffStrip();
-
-  delay(2000); // Wait for 2 seconds
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+    if (command == '1') {
+      // Turn on the RGB strip
+      Serial.println("Turning on lights");
+      turnOnStrip();
+    } else if (command == '0') {
+      // Turn off the RGB strip
+      Serial.println("Turning off lights");
+      turnOffStrip();
+    }
+  }
 }
 
-
 void turnOnStrip() {
-  strip.fill(strip.Color(0,0, 255));
+  strip.setBrightness(100);
+  for (int i = 0; i < NUMPIXELS; i++) {
+    strip.setPixelColor(i, 0, 255, 0); // Set color to red (adjust values for your desired color)
+  }
   strip.show();
 }
 
 void turnOffStrip() {
-  strip.clear();
+  for (int i = 0; i < NUMPIXELS; i++) {
+    strip.setPixelColor(i, 0, 0, 0); // Set color to off
+  }
   strip.show();
+  delay(10);
 }
