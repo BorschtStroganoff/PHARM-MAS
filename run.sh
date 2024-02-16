9a8d9fb3083f8d535c1fedc5cd90a8a967b06156
Nickolas Azanov <144555524+BorschtStroganoff@users.noreply.github.com>
2024-02-15 19:52:18 -0500

    Create run.sh
    This is the .sh file that will run the python script

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Run your Python script
+python your_script.py
+
+# Deactivate virtual environment (optional)
+deactivate
+
+# Type this into bash
+
+# chmod +x run.sh
+
+# To run the script, type
+# ./run.sh
